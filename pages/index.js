955112da2dd8a46529346f9b0f0a88b41e7eafaa
Phillip Oehlenberg <hi@phillip-oehlenberg.com>
2024-07-31 11:18:59 +0200

    Finish Home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,15 +10,24 @@ export default function Home() {
           src="/logo_icon_ani_complete_small.webp"
           alt="header-animation"
           fill
+          priority
+          unoptimized
         />
       </ImageWrapper>
       <Headline>
         HEY THERE, <HeadlineHighlight>I&apos;M PHILLIP.</HeadlineHighlight> NICE
         TO MEET YOU!
       </Headline>
+      <StyledLink href="/profile">Go to Profile &gt;&gt;&gt;</StyledLink>
     </HeaderContainer>
   );
 }
+
+const breatheAnimation = keyframes`
+0% {letter-spacing: 0;}
+50% {letter-spacing: 0.03rem;}
+100% {letter-spacing: 0;}
+`;
 
 const HeaderContainer = styled.div`
   margin: 0 auto;
@@ -25,7 +35,7 @@ const HeaderContainer = styled.div`
   right: 0;
   width: 92vw;
   margin-top: 7rem;
-  margin-bottom: 6rem;
+  margin-bottom: 7rem;
   position: static;
   display: grid;
   grid-template-rows: auto auto;
@@ -56,4 +66,16 @@ const Headline = styled.h1`
 const HeadlineHighlight = styled.span`
   color: #fff;
   -webkit-text-stroke: 2px #000;
+`;
+
+const StyledLink = styled(Link)`
+  position: relative;
+  top: 2.5rem;
+  font-size: min(4.5vw, 1.5rem);
+  font-weight: 500;
+  text-decoration: none;
+  color: #000;
+  animation-name: ${breatheAnimation};
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
 `;
