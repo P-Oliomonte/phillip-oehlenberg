21e52fced0b4e8939161f70cdae8e20094abb63d
Phillip Oehlenberg <hi@phillip-oehlenberg.com>
2024-08-08 13:07:46 +0200

    Attempt to avoid avoid auto detection of phone numbers

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
 import useLocalStorageState from "use-local-storage-state";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   const [page, setPage] = useLocalStorageState("page", {
@@ -12,8 +13,13 @@ export default function App({ Component, pageProps }) {
   }
 
   return (
-    <Layout page={page} onPageChange={handlePageChange}>
-      <Component {...pageProps} onPageChange={handlePageChange} />
-    </Layout>
+    <>
+      <Head>
+        <meta name="format-detection" content="telephone=no" />
+      </Head>
+      <Layout page={page} onPageChange={handlePageChange}>
+        <Component {...pageProps} onPageChange={handlePageChange} />
+      </Layout>
+    </>
   );
 }
