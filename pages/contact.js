4f63cc8bc70c502c75b1af83f3c40fd4dc1b19a4
Phillip Oehlenberg <hi@phillip-oehlenberg.com>
2024-08-08 14:16:18 +0200

    Change contact

--- pages/contact.js
+++ pages/contact.js
@@ -12,7 +12,7 @@ export default function Contact({ onPageChange }) {
       <IconWrapper>
         <Image src="/Icon_Contact.svg" fill alt="icon_profile" />
       </IconWrapper>
-      <HeadlineProfile>CONTACT</HeadlineProfile>
+      <HeadlineContact>CONTACT</HeadlineContact>
       <ContactIntro>
         If you are interested in working with me please feel free to contact me.
         I&apos;m looking forward to getting to know you.
@@ -105,7 +105,7 @@ const IconWrapper = styled.div`
   grid-row: 1 / 2;
 `;
 
-const HeadlineProfile = styled.h1`
+const HeadlineContact = styled.h1`
   position: relative;
   animation-name: ${headlineAnimation};
   animation-duration: 0.5s;
